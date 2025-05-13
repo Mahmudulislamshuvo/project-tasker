3f59f0c546ce6fe87449e66782bbf179adf2999b
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-05-14 00:30:44 +0600

    Static design done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Nav";
 import HeroSection from "./Components/HeroSection/Hero";
 import TaskBoard from "./Components/TaskBoard/TaskBoard";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-[#191D26] font-[Inter] text-white">
       <Header />
-      <HeroSection />
-      <TaskBoard />
+      <div className="flex flex-col items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
       <Footer />
     </div>
   );
--- src/Components/TaskBoard/TaskBoard.jsx
+++ src/Components/TaskBoard/TaskBoard.jsx
@@ -1,5 +1,24 @@
+import ActionButtons from "./ActionButtons";
+import Searchbar from "./Searchbar";
+import TaskList from "./TaskList";
+
 const TaskBoard = () => {
-  return <div>TaskBoard</div>;
+  return (
+    <>
+      <div class="mb-20" id="tasks">
+        <div class="container">
+          <div class="p-2 flex justify-end">
+            <Searchbar />
+          </div>
+
+          <div class="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+            <ActionButtons />
+            <TaskList />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TaskBoard;
