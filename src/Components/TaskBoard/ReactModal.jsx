08d2ecd5d40640d3f1cb2be0677d1ea2662e832d
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-05-14 05:47:49 +0600

    modal added with some others work

---
+++ src/Components/TaskBoard/ReactModal.jsx
@@ -0,0 +1,110 @@
+import { IoIosCloseCircleOutline, IoMdCloseCircle } from "react-icons/io";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+const ReactModals = ({
+  openModal,
+  afterOpenModal,
+  closeModal,
+  modalIsOpen,
+}) => {
+  return (
+    <>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal}>
+          <IoMdCloseCircle className="w-5 h-5 text-white rounded-full bg-red-500 cursor-po" />
+        </button>
+        <div>I am a modal</div>
+        <form>
+          <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]"></h2>
+
+          <div className="space-y-9 text-white lg:space-y-10">
+            <div className="space-y-2 lg:space-y-3">
+              <label htmlFor="title">Title</label>
+              <input
+                className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+                type="text"
+                name="title"
+                id="title"
+                required
+              />
+            </div>
+
+            <div className="space-y-2 lg:space-y-3">
+              <label htmlFor="description">Description</label>
+              <textarea
+                className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
+                type="text"
+                name="description"
+                id="description"
+                required
+              ></textarea>
+            </div>
+
+            <div className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
+              <div className="space-y-2 lg:space-y-3">
+                <label htmlFor="tags">Tags</label>
+                <input
+                  className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+                  type="text"
+                  name="tags"
+                  id="tags"
+                  required
+                />
+              </div>
+
+              <div className="space-y-2 lg:space-y-3">
+                <label htmlFor="priority">Priority</label>
+                <select
+                  className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
+                  name="priority"
+                  id="priority"
+                  required
+                >
+                  <option value="">Select Priority</option>
+                  <option value="Low">Low</option>
+                  <option value="Medium">Medium</option>
+                  <option value="High">High</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-16 flex justify-between lg:mt-20">
+            <button
+              onClick={closeModal}
+              className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80"
+            >
+              Close
+            </button>
+            <button
+              type="submit"
+              className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </Modal>
+    </>
+  );
+};
+
+export default ReactModals;
--- src/Components/TaskBoard/TaskBoard.jsx
+++ src/Components/TaskBoard/TaskBoard.jsx
@@ -1,19 +1,54 @@
+import { useState } from "react";
 import ActionButtons from "./ActionButtons";
 import Searchbar from "./Searchbar";
 import TaskList from "./TaskList";
 
 const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learning React",
+    description:
+      "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
+    tegs: ["React", "Nextjs", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [Task, setTask] = useState([defaultTask]);
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const onAddtoCart = () => {
+    setIsOpen(true);
+  };
   return (
     <>
-      <div class="mb-20" id="tasks">
-        <div class="container">
-          <div class="p-2 flex justify-end">
+      <div className="mb-20" id="tasks">
+        <div className="container">
+          <div className="p-2 flex justify-end">
             <Searchbar />
           </div>
 
-          <div class="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-            <ActionButtons />
-            <TaskList />
+          <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+            <ActionButtons
+              openModal={openModal}
+              afterOpenModal={afterOpenModal}
+              closeModal={closeModal}
+              modalIsOpen={modalIsOpen}
+              DoingSomething={onAddtoCart}
+            />
+            <TaskList Tasks={Task} />
           </div>
         </div>
       </div>
