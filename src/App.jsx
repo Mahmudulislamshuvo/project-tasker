f0af90c346fadfd09ccabbdc8ceaf12774f4f4f7
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-05-13 19:37:57 +0600

    static setup copied

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Nav";
+import HeroSection from "./Components/HeroSection/Hero";
+import TaskBoard from "./Components/TaskBoard/TaskBoard";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      <TaskBoard />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
